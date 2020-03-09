33bbcba2288f74fbb3a97364c40cd0dc4e6164d6
olmos <olmosescolatreball@gmail.com>
2020-03-09 13:41:13 +0100

    conexion ok flata new scene

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    public Connection conn;
-    public PreparedStatement pst = null;
-    public ResultSet resultSet = null;
+
     @FXML
     Button btnSignin;
     @FXML
@@ -29,7 +32,10 @@ public class Controller implements Initializable {
 
     }
 
-    public static Connection databaseConneciton() {
+    public Scene loginScene, mainWindowsScene;
+    public Stage primaryStage = null;
+
+    public static Connection databaseConneciton(String user, String password) {
         Connection conexion = null;
         try {
             //Cargar el driver
@@ -37,7 +43,6 @@ public class Controller implements Initializable {
             // Establecemos la conexion con la BD
             conexion = DriverManager.getConnection
                     ("jdbc:postgresql://localhost/dbtest", "adminadmin", "adminadmin");
-            return conexion;
         } catch (ClassNotFoundException e) {
             e.getMessage();
         } catch (SQLException e) {
@@ -46,66 +51,61 @@ public class Controller implements Initializable {
         return conexion;
     }
 
-
-
-    public void logIn() {
-        if ((txtUser.getText().isEmpty() || txtPassword.getText().isEmpty())) {
-            mostraDialog(Alert.AlertType.WARNING, "falten camps per omplir", null, "TILUL");
-            lbl_error.setText("tens que omplir els camps");
-        }
-
-
+    public Boolean logIn() {
+        ResultSet resultSet = null;
+        Statement sentencia = null;
+        Connection conexion = null;
         String user = txtUser.getText().toString();
         String password = txtPassword.getText().toString();
-
-
+        conexion = databaseConneciton(user, password);
         try {
-            //Cargar el driver
-            Class.forName("org.postgresql.Driver");
-            // Establecemos la conexion con la BD
-            Connection conexion = DriverManager.getConnection
-                    ("jdbc:postgresql://localhost/dbtest", "adminadmin", "adminadmin");
-
             // Preparamos la consulta
-            Statement sentencia = conexion.createStatement();
-            ResultSet resultSet = sentencia.executeQuery("SELECT * FROM users where username = '" + user + "' and password = '" + password + "';");
-
-
-            if (!resultSet.next()) {
-                lbl_error.setText("usuari o password mal introduit");
-                System.err.println("Password mal introduit");
+            sentencia = conexion.createStatement();
+            resultSet = sentencia.executeQuery("SELECT * FROM users where username = '" + user + "' and password = '" + password + "';");
+            if ((txtUser.getText().isEmpty() || txtPassword.getText().isEmpty())) {
+                //mostraDialog(Alert.AlertType.WARNING, "falten camps per omplir", null, "TILUL");
+                lbl_error.setText("tens que omplir els camps");
+                System.err.println("tens que omplir els camps");
+                return false;
 
             } else {
-                System.out.println("Login correcte");
-                mostraDialog(Alert.AlertType.CONFIRMATION, "Login Correcte, benvingut " + user, null, null);
+                if (!resultSet.next()) {
+                    lbl_error.setText("usuari o password mal introduit");
+                    System.err.println("Password mal introduit");
+                    return false;
+                } else {
+                    lbl_error.setText("");
+                    System.out.println("Login correcte");
+                    mostraDialog(Alert.AlertType.CONFIRMATION, "Login Correcte, benvingut " + user, null, null);
 
+                    Parent root = FXMLLoader.load(getClass().getResource("appInside.fxml"));
+                    primaryStage.setTitle("");
+                    primaryStage.setScene(new Scene(root, 400, 275));
+                    primaryStage.show();
+                    return true;
+
+                }
             }
 
-
-            resultSet.close();// Cerrar ResultSet
-            sentencia.close();// Cerrar Statement
-            conexion.close();//Cerrar conexion
-
-        } catch (ClassNotFoundException cn) {
-            cn.printStackTrace();
-        } catch (SQLException e) {
+        } catch (
+                SQLException e) {
+            e.getMessage();
+        } catch (IOException e) {
             e.printStackTrace();
-        }
+        } finally {
+            try {
+                resultSet.close();// Cerrar ResultSet
+                sentencia.close();// Cerrar Statement
+                conexion.close();//Cerrar conexion
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
-//        try {
-//            Parent appinside = FXMLLoader.load(getClass().getResource("appInside.fxml"));
-//            Scene scene = new Scene(appinside);
-//
-//            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//
-//            window.setScene(scene);
-//
-//            window.show();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        }
+        return null;
     }
+
+
 
     public void mostraDialog(Alert.AlertType alertType, String info, String capcelera, String titul) {
         Alert alert = new Alert((alertType));
