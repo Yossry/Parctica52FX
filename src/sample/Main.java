1e5c31de8c718f9dd515164e3e26626a384279b0
olmos <olmosescolatreball@gmail.com>
2020-03-13 16:10:44 +0100

    imagenes  and working

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 380));
         primaryStage.show();
     }
 
